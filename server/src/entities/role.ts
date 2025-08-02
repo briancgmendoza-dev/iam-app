10403d52c9170a284994f0666d3c185f88460dc4
Brian Christopher Mendoza <briancgmendoza@gmail.com>
2025-08-02 23:21:16 +0800

    [Feature] Entities

---
+++ server/src/entities/role.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
+import { Group } from './group';
+
+@Entity()
+export class Role {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  name!: string;
+
+  @Column({ nullable: true })
+  description?: string;
+
+  @ManyToMany(() => Group, (group) => group.roles)
+  groups!: Group[];
+}
